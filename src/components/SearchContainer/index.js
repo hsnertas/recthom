37ab2bbd3e7c3437e531dc257df42d344832478c
Hasan Ertas <hsnertas@gmail.com>
2020-07-26 23:25:25 -0400

    table datat added

--- src/components/SearchContainer/index.js
+++ src/components/SearchContainer/index.js
@@ -9,13 +9,18 @@ import API from"../../utils/API"
 
 
 export default class SearchContainer extends Component {
-   
-   
-    state={
-        results: []
+   constructor(props) {
+       super(props);
+       this.state = {
+           results: [],
+           order: "descend"
+       }
+       this.tableHeadSort = this.tableHeadSort();
+   }
+    tableHeadSort=()=>{
+        (this.state.order === "descend") ? this.setState({order: "ascend"}) : this.setState({order: "descend"})
+        console.log(this.state.order);
     }
-   
-
         tableHeads = [
         { name: "Image"},
         { name: "Name"},
@@ -23,9 +28,6 @@ export default class SearchContainer extends Component {
         { name: "Email"},
         { name: "DOB"}
         ]
-
-
-
 
 
    componentDidMount (){
@@ -40,12 +42,15 @@ export default class SearchContainer extends Component {
    
    
     render() {
-     
+  
+
         return (
             <div>
+            
                 <Navbar></Navbar>
                 <Table results={this.state.results}
                 tableHeads={this.tableHeads}
+                tableHeadSort={this.tableHeadSort}
                 />
             </div>
         )
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,25 +1,32 @@
 import React from 'react'
 import "./style.css"
+import TableData from '../TableData'
  function Table(props) {
     return (
         <div>
-         {console.log(props.tableHeads)}
-
+        
+        
          <table>
              <thead>
                  <tr>
-                  {props.tableHeads.map(tableHead=>{
+                  {props.tableHeads.map((tableHead)=>{
                      console.log("test");
-                  return<th key={tableHead.name}>{tableHead.name}</th>
+                  return<th key={tableHead.name}
+                  onClick={()=>{
+                    props.tableHeadSort()
+                }}
+                  
+                  >{tableHead.name} </th>
+                }
 
-                                  
-                  }
-
-                  )}
+                )}
 
 
                  </tr>
              </thead>
+                <TableData results={props.results}
+                 
+                />
          </table>
         </div>
     )
